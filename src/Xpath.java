7857dbdda81434e68955db6204da729315fd7fcb
Ciro(Guangcheng Wen) <cero9406@gmail.com>
2022-10-18 23:56:42 -0700

    added Xpath learning agenda but encountering bugs that chrome extensions ChroPath and SelectorHub are not showing in DevTools window

---
+++ src/Xpath.java
@@ -0,0 +1,17 @@
+/**
+ * 1. What is Xpath
+ * XML Path, the address of the element using the HTML DOM structure.
+ * 2. How to locate the element using the Xpath
+ * 3. About ChroPath Plugin
+ * 4. Types of Xpath
+ * 5. Syntax of Xpath
+ * 6. Difference between absolute and relative Xpath
+ * 7. Out of absolute and relative Xpath which one is preferred and why?
+ *
+ */
+public class Xpath {
+    public static void main(String[] args){
+
+
+    }
+}
